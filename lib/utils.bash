74e24bc595d9e40b5af93259e0759fa44e092bbe
Sylvain <s.metayer@groupeonepoint.com>
2024-04-10 22:19:01 +0200

    feat: initial commit

--- lib/utils.bash
+++ lib/utils.bash
@@ -20,29 +20,28 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 fi
 
 sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+	awk -F/go '{ print $2 }' |
+		uniq |
+		sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n |
+		grep -v '^1\($\|\.0\|\.0\.[0-9]\|\.1\|\.1rc[0-9]\|\.1\.[0-9]\|.2\|\.2rc[0-9]\|\.2\.1\|.8.5rc5\)$' |
+		tr '\n' ' '
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if go has other means of determining installable versions.
-	list_github_tags
+	git ls-remote --tags --refs "$GH_REPO" "go*"
 }
 
 download_release() {
 	local version filename url
 	version="$1"
 	filename="$2"
+	local platform=""
+	local arch=""
 
-	# TODO: Adapt the release URL convention for go
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	platform=$(get_platform)
+	arch=$(get_arch)
+
+	url="https://dl.google.com/go/go${version}.${platform}-${arch}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -51,7 +50,7 @@ download_release() {
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="${3%/bin}"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -61,14 +60,45 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert go executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
 		rm -rf "$install_path"
 		fail "An error occurred while installing $TOOL_NAME $version."
 	)
+}
+
+get_platform() {
+	local platform=""
+
+	platform="$(uname | tr '[:upper:]' '[:lower:]')"
+
+	case "$platform" in
+	linux | darwin | freebsd) ;;
+	*)
+		fail "Platform '${platform}' not supported!"
+		;;
+	esac
+
+	printf "%s" "$platform"
+}
+
+get_arch() {
+	local arch=""
+	local arch_check=${ASDF_GOLANG_OVERWRITE_ARCH:-"$(uname -m)"}
+	case "${arch_check}" in
+	x86_64 | amd64) arch="amd64" ;;
+	i686 | i386 | 386) arch="386" ;;
+	armv6l | armv7l) arch="armv6l" ;;
+	aarch64 | arm64) arch="arm64" ;;
+	ppc64le) arch="ppc64le" ;;
+	*)
+		fail "Arch '${arch_check}' not supported!"
+		;;
+	esac
+
+	printf "%s" "$arch"
 }
